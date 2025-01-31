51822ee759b7b6d2f7e7df76e9b979f6198741b8
Paris Rainey <paris.c.rainey@gmail.com>
2025-01-31 15:23:01 -0600

    Update: Added Target to AntBehavior

--- Assets/_Project/Core/Scripts/AntBehavior.cs
+++ Assets/_Project/Core/Scripts/AntBehavior.cs
@@ -8,10 +8,17 @@ public class AntBehavior : MonoBehaviour
     private float _health = 100;
     [SerializeField]
     private bool _isVariant = false;
+    [SerializeField]
+    private GameObject _target;
 
     public float Health
     { 
         get { return _health; } 
         set { _health = value; } 
+    }
+
+    public bool Variant 
+    { 
+        get { return _isVariant; } 
     }
 }
