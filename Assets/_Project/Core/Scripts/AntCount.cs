6a1e5f0fdefc6d8f479e9121d0c8533e1deefa30
Haidon Cosse <Haidonmcosse@gmail.com>
2025-02-06 11:08:42 -0600

    fdsf

---
+++ Assets/_Project/Core/Scripts/AntCount.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+
+public class AntCount : MonoBehaviour
+{
+    [SerializeField] 
+    private TextMeshProUGUI AntCountTextBox;
+    [SerializeField]
+    private AntBehavior Behavior;
+
+    private void Update()
+    {
+        AntCountTextBox.text = "Ants Exterminated!" + Behavior.Extermination.ToString();
+    }
+}
