9485d81ff67aa5e56a9f487d7068084d492a52c4
Reginald Thompson <reginaldjthompsonjr23@gmail.com>
2025-02-14 16:59:58 -0600

    Begun work on sugar cube.

--- Assets/_Project/Core/Scripts/SugarBomb.cs
+++ Assets/_Project/Core/Scripts/SugarBomb.cs
@@ -1,9 +1,17 @@
+using HurricaneVR.Framework.Core.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SugarBomb : MonoBehaviour
 {
+
+    [SerializeField]
+    private int timeToDestroy;
+
+    private GameObject[] distractedAnts = new GameObject[200];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,8 @@ public class SugarBomb : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //if (Vector3.Distance(GameObject.FindGameObjectWithTag("Ants").transform.position,
         
     }
 }
