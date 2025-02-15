596e49ea6fc3dc45e1cd5b417ac842cab720eee7
Reginald Thompson <s238002@students.aie.edu>
2025-02-14 23:55:14 -0600

    Added functional sugar cube in environment test scene

--- Assets/_Project/Core/Scripts/SugarBomb.cs
+++ Assets/_Project/Core/Scripts/SugarBomb.cs
@@ -1,6 +1,8 @@
 using HurricaneVR.Framework.Core.Utils;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -8,21 +10,82 @@ public class SugarBomb : MonoBehaviour
 {
 
     [SerializeField]
-    private int timeToDestroy;
+    private float timeUntilDespawn = 5;
+    
+    [SerializeField]
+    private float timeToRespawn = 10;
 
-    private GameObject[] distractedAnts = new GameObject[200];
+
+    private GameObject[] distractedAnts;
+
+    private bool isActive = false;
+
+    private bool isReady = false;
+
+    private Vector3 startingLocation;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingLocation = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        //if (Vector3.Distance(GameObject.FindGameObjectWithTag("Ants").transform.position,
+
+        if (isActive ==true)
+        {
+            timeUntilDespawn -= Time.deltaTime;
+            Attract();
+        }
+
+        if (timeUntilDespawn <= 0)
+        {
+            ResetAnts();
+            isActive = false;
+            isReady = false;
+        }
+
+        if (isReady == false)
+        {
+            timeToRespawn -= Time.deltaTime;
+        }
+
+        if (timeToRespawn <= 0)
+        {
+            isReady = true;
+            timeUntilDespawn = 5;
+            timeToRespawn = 10;
+            transform.localPosition = startingLocation;
+        }
+
+    }
+
+    public void Attract()
+    {
+        isActive = true;
+        distractedAnts = GameObject.FindGameObjectsWithTag("Ants");
+        for (int i =0; i<distractedAnts.Length; i++)
+        {
+            GameObject ant = distractedAnts[i];
+            ant.GetComponent<AntBehavior>().Target = GameObject.FindGameObjectWithTag("SugarCube");
+
+            
+        }
+    }
+
+    public void ResetAnts()
+    {
+
+        for (int i = 0; i < distractedAnts.Length; i++)
+        {
+            GameObject ant = distractedAnts[i];
+            ant.GetComponent<AntBehavior>().Target = GameObject.FindGameObjectWithTag("Player");
+
+        }
         
     }
 }
